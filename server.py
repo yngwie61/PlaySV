954b07c11c720c9553d81a233761ea25e45b3528
rerere007 <rock61202416@gmail.com>
2024-09-09 00:50:02 +0900

    initial commit sample video server

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+def run(server_class=HTTPServer, handler_class=CORSHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
+
